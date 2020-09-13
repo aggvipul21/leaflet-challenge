5a78d7b52593421016cc3ff317512b112e0a61bf
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-09-13 14:28:01 -0400

    Changed marker color and color based on magnitude; updated popup to show magnitude in addition to date and place

--- static/js/Leaflet-Step-1/logic.js
+++ static/js/Leaflet-Step-1/logic.js
@@ -1,44 +1,80 @@
+//function to create map
 function createMap(earthquakes){
-
- // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
- var streetmap= L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  })
-
 
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
+    //layers: [streetmap, earthquakes]
+    });
+
+ // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+ var geojsonMarkerOptions = {
+    //fillcolor:"#DF3A01",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3> Location:<hr>" + feature.properties.place +
+      "</h3><hr><p>Date:<hr>" + new Date(feature.properties.time) + 
+      "</p><hr><p>Magnitude:<hr>" + feature.properties.mag + "</p>");
+  }
+
+function markercolor(magnitude){
+
+    switch(true){
+
+        case (magnitude<=1):
+            return "#A5DF00";
+        case (magnitude>1 && magnitude<=2):
+            return "#D7DF01";
+        case (magnitude>2 && magnitude<=3):
+            return "#DBA901";
+        case (magnitude>3 && magnitude<=4):
+            return "#DF7401";
+        case (magnitude>4 && magnitude<=5):
+            return "#DF3A01";
+        default:
+            return "#DF0101";
+
+    }
 
 }
 
-function createFeatures(earthquakeData) {
+var streetmap= L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/streets-v11",
+accessToken: API_KEY
+}).addTo(myMap)
 
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-  
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
-  }
+
+// Create a GeoJSON layer containing the features array on the earthquakeData object
+// Run the onEachFeature function once for each piece of data in the array
+// var earthquakes = 
+L.geoJSON(earthquakes, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    style: function(feature) {
+        return{
+            radius:feature.properties.mag*4
+            ,fillColor:markercolor(feature.properties.mag)
+        }
+    },
+    onEachFeature:onEachFeature
+}).addTo(myMap);
+
+
+}
+
+//Call earthquake data for last 7 days and build on map
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", data=>{
     console.log(data)
-    createFeatures(data.features);
+    createMap(data.features);
 });
